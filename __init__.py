34a0e47de0e56a1c04c0a6d7ba566bb2aea33a46
Adrian <marin.mag@stx.ox.ac.uk>
2023-06-12 14:58:59 +0100

    Simplified __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,3 @@
-__all__ = ["analyze_output", "element_output", "obspyfy", "station_output"]
-
-from station_output import station_output
-from element_output import element_output
+from .station_output import station_output
+from .element_output import element_output
+from .obspyfy import obspyfy
