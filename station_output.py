070f1a6fa7237b3bc3410a83c754fa2c6db69159
Adrian <marin.mag@stx.ox.ac.uk>
2023-06-23 15:05:15 +0100

    changed station_output to deal with all outputs and to fit in the new meta

--- station_output.py
+++ station_output.py
@@ -8,27 +8,109 @@ from obspy import UTCDateTime
 import pandas as pd
 import netCDF4 as nc
 import os 
+import yaml
+
+from .axisem3d_output import AxiSEM3DOutput
 
 #@@@@@@@@@@
 # HANDLER @
 #@@@@@@@@@@
 
-class station_output:
-    def __init__(self, path: str):
+class StationOutput(AxiSEM3DOutput):
+    def __init__(self, path_to_station_output: str):
         """Class for handling axisem station output in netcdf format. 
 
         Args:
             path (str): Path to station output directory of simultaion
         """
-        self.data = xr.open_mfdataset(path+"/axisem3d_synthetics.nc.*", 
-                                      engine="netcdf4", 
-                                      data_vars="different", 
-                                      concat_dim="dim_station", 
-                                      combine="nested")
-        self.station_names_decoded = np.array([ls.decode("utf-8") for ls in self.data["list_station"].values])
+        path_to_simulation = self._find_simulation_path(path_to_station_output)
+        super().__init__(path_to_simulation)
+
+        self.path_to_station_output = path_to_station_output
+        self.station_group_name = os.path.basename(path_to_station_output)
+        # Load output files
+        pattern = 'axisem3d_synthetics.nc.rank' 
+        self.files = [f for f in os.listdir(path_to_station_output) if pattern in f]
+        self.nc_data = {filename: nc.Dataset(self.path_to_station_output + '/' + filename) \
+                        for filename in self.files}
+        # read the rank list file
+        self.rank_list = pd.read_csv(path_to_station_output + '/rank_station.info',
+                                     sep=' ', header=None, 
+                                     names=['MPI_RANK', 'STATION_KEY', 'STATION_INDEX_IN_RANK'])
+        # Load metadata
+        self.data_time =  self._get_time()
+        with open(self.inparam_output, 'r') as file:
+            output_yaml = yaml.load(file, Loader=yaml.FullLoader)
+            station_group = output_yaml['list_of_station_groups'][0][self.station_group_name]
+            self.coordinate_frame = station_group['wavefields']['coordinate_frame']
+            self.channels = station_group['wavefields']['channels']        
+        self.detailed_channels = self._get_detailed_channels()
+        self.detailed_channels = [element.replace('1', self.coordinate_frame[0]).\
+                                  replace('2', self.coordinate_frame[1]).\
+                                  replace('3', self.coordinate_frame[2]) \
+                                  for element in self.detailed_channels]
+        
+
+    def load_data_at_station(self, network: str, 
+                          station_name: str, 
+                          channels: list = None,
+                          time_limits: list = None):
+        # Form the station key
+        station_key = network + '.' + station_name 
+        # Find the file and location within the file of the station
+        row = self.rank_list[self.rank_list['STATION_KEY'] == station_key] 
+        rank_index = row.values.tolist()[0][0]
+        in_file_index = row.values.tolist()[0][2]
+        filename = 'axisem3d_synthetics.nc.rank'  + str(rank_index)
+        # get wave data for this rank
+        wave_data = nc.Dataset(self.path_to_station_output + '/' + filename)['data_wave'][in_file_index].filled()
+
+        if channels is None and time_limits is None:
+            return wave_data
+        elif channels is None and time_limits is not None:
+            # Check if times desired are available
+            if (np.min(self.data_time) < time_limits[0] and 
+                np.max(self.data_time) > time_limits[1]):
+                # Find the indices of elements between t_min and t_max
+                indices = np.where((self.data_time >= time_limits[0]) & 
+                                   (self.data_time <= time_limits[1]))
+                return wave_data[:,indices]
+            else:
+                raise Exception('Times must be between: ' + 
+                                str(min(self.data_time)) + 
+                                ' and ' + str(max(self.data_time)))
+        elif channels is not None and time_limits is None:
+            if(self._check_elements(channels, self.channels)):
+                # Filter by channels chosen
+                channel_indices = []
+                for channel in channels:
+                    channel_indices += [index for index, element in enumerate(self.detailed_channels) if element.startswith(channel)]
+                return wave_data[channel_indices,:]
+            else:
+                raise Exception('Only the following channels exist: ' + ', '.join(self.channels))
+        else:            
+            # Check if times desired are available
+            if np.min(self.data_time) < time_limits[0] and np.max(self.data_time) > time_limits[1]:
+                # Find the indices of elements between t_min and t_max
+                indices = np.where((self.data_time >= time_limits[0]) & (self.data_time <= time_limits[1]))
+                wave_data = wave_data[:,indices]
+            else:
+                raise Exception('Times must be between: ' + str(min(self.data_time)) + ' and ' + str(max(self.data_time)))
+            if(self._check_elements(channels, self.channels)):
+                # Filter by channels chosen
+                channel_indices = []
+                for channel in channels:
+                    channel_indices += [index for index, element in enumerate(self.detailed_channels) if element.startswith(channel)]
+                return wave_data[channel_indices,:]
+            else:
+                raise Exception('Only the following channels exist: ' + ', '.join(self.channels))
 
 
-    def station_to_stream(self, networks: list, station_names: list, locations: list, channels_list: list, data_type='RTZ') -> obspy.Stream:
+    def stream(self, networks: list, 
+                          station_names: list, 
+                          locations: list = None, 
+                          channels: list = None,
+                          time_limits: list = None) -> obspy.Stream:
         """Returns stream with data from the chosen stations and channels
 
         Args:
@@ -41,148 +123,142 @@ class station_output:
         Returns:
             obspy.Stream: Obspy Stream of data 
         """
-        channel_indices = []
-        for channels in channels_list:
-            channel_indices.append([data_type.find(x) for x in channels])
+        
         # Initialize stream that will hold all the traces
         stream = obspy.Stream()
         # Compute sampling delta (it is the same for all traces) and no of points per trace
-        delta = self.time[1] - self.time[0]
-        npts = len(self.time)
+        delta = self.data_time[1] - self.data_time[0]
+        npts = len(self.data_time)
         # Form full station name from components given (not including channel in the name)
-        for i in range(len(networks)):
-            network = networks[i]
+        for i, network in enumerate(networks):
             station_name = station_names[i]
-            location = locations[i]
-            
-            for ichn, chn in enumerate(channel_indices[i]):
+            if locations is None:
+                location = ''
+            else:
+                location = locations[i]
+
+            if channels is not None:
+                selected_detailed_channels = [element for element in self.detailed_channels \
+                                            if any(element.startswith(prefix) for prefix in channels)]
+            else:
+                selected_detailed_channels = self.detailed_channels
+            for chn_index, chn in enumerate(selected_detailed_channels):
                 if location == '':
-                    full_name = str(network) + '.' + str(station_name)
-                    index = self._find_station_index(full_name)
                     # extract data
-                    displacement_data = self.data["data_wave"].values[index, chn, :]
+                    wavefield_data = self.load_data_at_station(network, station_name, channels, time_limits)
                     # form obspy trace
-                    trace = obspy.Trace(displacement_data)
+                    trace = obspy.Trace(wavefield_data[chn_index])
                     trace.stats.delta = delta
                     trace.stats.ntps = npts
                     trace.stats.network = network
                     trace.stats.station = station_name
                     trace.stats.location = location
-                    trace.stats.channel = 'LX' + channels[ichn]
+                    trace.stats.channel = chn
                     trace.stats.starttime = self.starttime
                     stream.append(trace)
                 else:
-                    full_name = str(network) + '.' + str(station_name) + '_' + str(location)
-                    index = self._find_station_index(full_name)
                     # extract data
-                    displacement_data = self.data["data_wave"].values[index, chn, :]
+                    wavefield_data = self.load_data_at_station(network, station_name, channels, time_limits)
                     # form obspy trace
-                    trace = obspy.Trace(displacement_data)
+                    trace = obspy.Trace(wavefield_data[chn_index])
                     trace.stats.delta = delta
                     trace.stats.ntps = npts
                     trace.stats.network = network
                     trace.stats.station = station_name
                     trace.stats.location = location
-                    trace.stats.channel = channels[ichn]
+                    trace.stats.channel = chn
                     trace.stats.starttime = self.starttime
                     stream.append(trace)
 
         return stream
 
 
-    @property
-    def time(self) -> np.ndarray:
+    def _get_time(self) -> np.ndarray:
         """Time axis of waveform
 
         Returns:
             np.ndarray: time
         """        
-        return self.data['data_time'].values
-    
+        return next(iter(self.nc_data.values()))['data_time'][:].filled()
+
+
+    def _check_elements(self, list1, list2):
+        """Checks if all elements in list1 can be found in list2.
+
+        Args:
+            list1 (list): The first list.
+            list2 (list): The second list.
+
+        Returns:
+            bool: True if all elements in list1 are found in list2, False otherwise.
+            list: List of elements from list1 that are not found in list2.
+        """
+        missing_elements = [element for element in list1 if element not in list2]
+        if len(missing_elements) == 0:
+            return True
+        else:
+            return False
+
+
     @property
     def starttime(self) -> UTCDateTime:
         default_event_time = UTCDateTime("1970-01-01T00:00:00.0Z")
-        return default_event_time + self.time[0]      
+        return default_event_time + self.data_time[0]      
 
 
-    def _find_station_index(self, name: str) -> int:
-        """Findx index of station in the data structure
+    def _find_simulation_path(self, path: str):
+        """Takes in the path to a station file used for axisem3d
+        and returns a stream with the wavefields computed at all stations
 
         Args:
-            name (str): Full name of station (including net, loc)
+            path_to_station_file (str): path to station.txt file
 
         Returns:
-            int: Index of station
-        """        
-         # find indices of stations in the database 
-        index = (name==self.station_names_decoded).argmax()
-        if (index == 0) and (name != self.station_names_decoded[0]):
-            raise Exception('Station could not be found')
-        
-        return index
+            parent_directory
+        """
+        current_directory = os.path.abspath(path)
+        while True:
+            parent_directory = os.path.dirname(current_directory)
+            if os.path.basename(current_directory) == 'output':
+                return parent_directory
+            elif current_directory == parent_directory:
+                # Reached the root directory, "output" directory not found
+                return None
+            current_directory = parent_directory
 
-#@@@@@@@@@
-# PARSER @
-#@@@@@@@@@
 
-def parse_station_output(path: str) -> obspy.Stream:
-    """Function that takes the path to stations output, gathers the nc files 
-    with the output from there and parses them into an obspy stream object.
-    This could also be done with the station_output class, but less memory intensive
-    because it tackles one station output file at a time. It also bypasses a possible
-    problem with xarray concat function which seems to explode the RAM usage in certain
-    cases.
+    def _get_detailed_channels(self):
+        """ The existence of this function is the terrbile
+         consequence of the absence of a list of channels 
+          in the metadata of stations output. It's horrible """
 
-    Args:
-        path (str): path to the dir containing station output files
+        U = ['U1', 'U2', 'U3']
+        G = ['G11', 'G12', 'G13', 'G21', 'G22', 'G23', 'G31', 'G32', 'G33']
+        E = ['E11', 'E12', 'E13', 'E22', 'E23','E33']
+        S = ['S11', 'S12', 'S13', 'S22', 'S23', 'S33']
+        R = ['R1', 'R2', 'R3']
 
-    Returns:
-        obspy.Stream: just what it says
-    """    
-    # find all files with data in the given simulation dir
-    pattern = 'axisem3d_synthetics.nc.rank' 
-    matching_files = [f for f in os.listdir(path) if pattern in f]
+        detailed_channels = []
+        for channel_type in self.channels:
+            if channel_type == 'U':
+                detailed_channels.extend(U)
+            elif channel_type == 'G':
+                detailed_channels.extend(G)
+            elif channel_type == 'E':
+                detailed_channels.extend(E)
+            elif channel_type == 'S':
+                detailed_channels.extend(S)
+            elif channel_type == 'R':
+                detailed_channels.extend(R)
 
-    # read the rank list file
-    rank_list = pd.read_csv(path + '/rank_station.info', sep=' ', header=None, names=['MPI_RANK', 'STATION_KEY', 'STATION_INDEX_IN_RANK'])
-
-    # initiate stream object
-    stream = obspy.Stream()
+        return detailed_channels
     
-    # go file by file
-    for file in matching_files:
-        # get rank number of this file
-        rank_no = file.split('.')[-1].strip('rank')
-        
-        # get wave data for this rank
-        data = nc.Dataset(path + '/' + file)
-        
-        # get time for this rank (should be the same for all ranks
-        # but I don't want to load a dataset outside the loop just to get the time)
-        time = [float(t) for t in data['data_time'][:].data]
-        delta = time[1] - time[0]
-        npts = len(time)
-        
-        # get the list of statinos with this rank
-        current_rank_list = rank_list[rank_list['MPI_RANK'].str.contains(rank_no)]
-        # go station by station
-        for index, trace_of_station in enumerate(data['data_wave']):
-            station_key = current_rank_list.loc[current_rank_list['STATION_INDEX_IN_RANK'] == str(index), 'STATION_KEY'].iloc[0]
-            network = station_key.split('.')[0]
-            station_name = station_key.split('.')[-1]
-            print(station_name)
-            for chn_index, chn in enumerate(['LXR', 'LXT', 'LXZ']):
-                # form the traces at the channel level
-                trace = obspy.Trace(trace_of_station[chn_index].data)
-                trace.stats.delta = delta
-                trace.stats.ntps = npts
-                trace.stats.network = network
-                trace.stats.station = station_name
-                trace.stats.location = ''
-                trace.stats.channel = chn
-                trace.stats.starttime = UTCDateTime("1970-01-01T00:00:00.0Z") + time[0]
-                stream.append(trace)
 
-    path_to_main_simulation_dir = path.partition("output")[0]
-    name = path_to_main_simulation_dir.split('/')[-2]
-    stream.write(path_to_main_simulation_dir + 'output/obspyfied/' + name + '.mseed', format="MSEED") 
+    def parse_to_mseed(self, channels: list = None, time_limits: list = None):
+        station_key_list = self.rank_list['STATION_KEY'].tolist()[1:-1]
+
+        # Extract the string before and after the dot
+        networks = [key.split(".")[0] for key in station_key_list]
+        stations = [key.split(".")[1] for key in station_key_list]
+
+        return self.stream(networks, stations, channels=channels, time_limits=time_limits)
